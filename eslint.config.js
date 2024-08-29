80317e074704def2957e3992277132f3f002f5e2
pasolanki <Pooja.Solanki@skillsoft.com>
2024-08-30 04:49:02 +0530

    change

--- eslint.config.js
+++ eslint.config.js
@@ -10,10 +10,10 @@ module.exports = {
   //     "eslint:recommended", // Use recommended ESLint rules
   //     "plugin:react/recommended", // Use recommended rules for React
   //   ],
-  parserOptions: {
-    ecmaVersion: 12, // Use ECMAScript 2021 syntax
-    sourceType: "module", // Enable ECMAScript modules
-  },
+  //   parserOptions: {
+  //     ecmaVersion: 12, // Use ECMAScript 2021 syntax
+  //     sourceType: "module", // Enable ECMAScript modules
+  //   },
   rules: {
     "no-console": "warn", // Warn when console statements are used
     "no-unused-vars": ["error", { argsIgnorePattern: "^_" }], // Error for unused variables, but ignore variables starting with '_'
@@ -21,9 +21,9 @@ module.exports = {
     indent: ["error", 2], // Enforce 2-space indentation
     quotes: ["error", "single"], // Enforce single quotes
   },
-  settings: {
-    react: {
-      version: "detect", // Automatically detect the version of React
-    },
-  },
+  //   settings: {
+  //     react: {
+  //       version: "detect", // Automatically detect the version of React
+  //     },
+  //   },
 };
